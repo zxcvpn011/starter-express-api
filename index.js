e4ffece99317ec65ccb7164aa8b69b55c6ccfd0c
MahmoudY3c <wpsmahmoud25@gmail.com>
2023-03-07 13:52:24 -0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const config = require("dotenv").config();
+const express = require("express")
+const app = express();
+const path = require("path")
+const PORT = process.env.PORT
+
+app.use(express.static(__dirname))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.all("/salla", function (req, res) {
+  console.log(req.body);
+  res.status(200).send("ok");
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(PORT)
